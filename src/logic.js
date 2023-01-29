b4f3e99f92eedf11f3993bf33414965d129b55d1
PJiyush <jhamnani.1@iitj.ac.in>
2023-01-29 15:46:27 +0530

    Able to put image on canvas

--- src/logic.js
+++ src/logic.js
@@ -4,6 +4,14 @@ const inputFile = document.getElementById('inputFile')
 const prevContainer = document.getElementsByClassName('container')
 const img_preview = document.querySelector('.Image__Preview')
 
+
+let opCanvas = document.getElementById('opCanvas')
+let opCanvasContext = opCanvas.getContext('2d');
+
+var img = new Image()
+
+let val =0
+
 inputFile.onchange = ()=>{
     const file = inputFile.files[0]
     console.log(file);
@@ -13,7 +21,33 @@ inputFile.onchange = ()=>{
         reader.addEventListener('load',()=>{
             console.log(file.path);
             img_preview.setAttribute('src',file.path)
+            img.src = file.path
+            
         })
+        // reader.readAsDataURLsy(file)
         reader.readAsDataURL(file)
+        setTimeout(()=>{
+            var width = img_preview.clientWidth
+            var height = img_preview.clientHeight
+            opCanvas.width = width
+            opCanvas.height = height
+            console.log(opCanvas.width, opCanvas.height);
+            opCanvasContext.drawImage(img, 0,0, width, height);
+                    console.log("Image is being drawn");
+        },1000)
     }
 }
+
+
+
+
+
+
+
+// opCanvas.width = width
+// opCanvas.height = height
+
+// console.log(opCanvas.width, opCanvas.height);
+console.log(img);
+
+
