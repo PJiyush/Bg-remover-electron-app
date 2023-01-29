9374288faffb90d794389488486f1f958c478a83
PJiyush <jhamnani.1@iitj.ac.in>
2023-01-29 14:43:13 +0530

    Previewing of image is enabled

--- src/logic.js
+++ src/logic.js
@@ -1,7 +1,19 @@
 console.log("Hello");
 
 const inputFile = document.getElementById('inputFile')
+const prevContainer = document.getElementsByClassName('container')
+const img_preview = document.querySelector('.Image__Preview')
+
 inputFile.onchange = ()=>{
     const file = inputFile.files[0]
     console.log(file);
+    if (file) {
+        const reader = new FileReader()
+        img_preview.style.display = 'block'
+        reader.addEventListener('load',()=>{
+            console.log(file.path);
+            img_preview.setAttribute('src',file.path)
+        })
+        reader.readAsDataURL(file)
+    }
 }
